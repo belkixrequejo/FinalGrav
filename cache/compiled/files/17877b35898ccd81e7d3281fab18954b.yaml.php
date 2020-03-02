6dcdafdc974d59939cbd7ea491a4fade2b2a9ade
belkixrequejo <belkix.requejo@pucese.edu.ec>
2020-03-02 22:44:45 +0000

    grav

---
+++ cache/compiled/files/17877b35898ccd81e7d3281fab18954b.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/grav/user/config/security.yaml',
+    'modified' => 1583187181,
+    'data' => [
+        'salt' => 'FkhHasxXWo7Vqi'
+    ]
+];
